adc9c5275cf22c2bd0813d79ca2c785e6c9e1b1e
Grayson Head <grayson@graysonhead.net>
2022-04-22 16:33:11 -0500

    Fixing multiple bootloaders

--- deckchair/configuration.nix
+++ deckchair/configuration.nix
@@ -11,7 +11,7 @@
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+ 
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
