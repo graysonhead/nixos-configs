ce6b581bbb8ece834c68eeafbd0e1923fa74c0e4
Grayson Head <grayson@graysonhead.net>
2022-04-23 14:51:34 -0500

    Fixes

--- desktop-manager/plasma.nix
+++ desktop-manager/plasma.nix
@@ -5,7 +5,6 @@
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
     services.printing.enable = true;
-    sound.enable = true;
     hardware.pulseaudio.enable = true;
     hardware.pulseaudio.package = pkgs.pulseaudioFull;
     hardware.bluetooth.enable = true;
