4a9d5cfe2e8a6b7875027179c095546007015b8c
Grayson Head <grayson@graysonhead.net>
2022-04-23 14:36:36 -0500

    Adding hardware specific configuration

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "Grayson's NixOS Configurations";
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations = {
       deckchair = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          nixos-hardware.nixosModules.dell-xps-13-9370
           ./deckchair/configuration.nix
           ./modules/nix-flakes.nix
           ./modules/default-system-packages.nix
