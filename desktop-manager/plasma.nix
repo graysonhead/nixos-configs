2806a9b9dc731e4b4437cfc67cc0ffd1dd7d8e4f
Grayson Head <grayson@graysonhead.net>
2022-04-23 14:18:46 -0500

    Adding missing module

---
+++ desktop-manager/plasma.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ...}:
+
+{
+    services.xserver.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
+    services.printing.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        firefox
+        networkmanager-iodine
+        networkmanager-openvpn
+        networkmanager-openconnect
+    ]
+}
