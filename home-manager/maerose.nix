229a688aed7994b5bcb0f2fff7335ec373fab963
Grayson Head <grayson@graysonhead.net>
2022-06-22 19:40:52 -0500

    Adding update alias

--- home-manager/maerose.nix
+++ home-manager/maerose.nix
@@ -5,4 +5,10 @@
     zip
     vlc
   ];
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      update = "sudo nix-collect-garbage && sudo nixos-rebuild switch --flake github:graysonhead/nixos-configs";
+    };
+  };
 }
