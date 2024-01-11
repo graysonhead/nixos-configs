d0ba98eb95424d241392199527d98290037d77f4
Grayson Head <grayson@graysonhead.net>
2024-01-11 01:36:39 -0600

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,7 +7,7 @@ let
   deckchair = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbxjqdNMawqU4C99V0OqyMVFxZCUypUQn6mTLErg6Yi";
   notanipad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDc6PTFN/6mi/eY5lAl3IzdBFhl6ppUYZcJpUrTwEN8P";
   mombox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2POjia3nzKq4rWfndaxKPc7/6G8a+OLSmgOKBL+twp";
-  green = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtPfxp+ZnI7Nsa6SBzLUbmJ6LUbeuUNTYJ4o5sNJ7TZ";
+  green = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WTvOABaXAllVH7vq0bOkKbEuKZ6FKXNYDwJMSJ7Kw";
   blue = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/+eOT7BTvdV2SgWeObYOEuAMrLiMHFg21VzXX8Wlku";
   chromebook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYaZ5RV6vfa1wiWaFFC1pgspnzcvIb6yfnBhhVNe4U4";
   systems = [ ops nx1 factorio deckchair notanipad mombox green blue chromebook ];
