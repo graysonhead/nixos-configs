9dfb57f8ad7c423846e227dac286332205cd41f7
Grayson Head <grayson@graysonhead.net>
2025-08-09 19:35:25 -0500

    Updates

--- systems/notanipad/configuration.nix
+++ systems/notanipad/configuration.nix
@@ -45,6 +45,10 @@
     GDK_SCALE = "2";
     GDK_DPI_SCALE = "0.5";
     _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_USE_XINPUT2 = "1";
+    MOZ_DBUS_REMOTE = "1";
+    ELECTRON_FORCE_DEVICE_SCALE_FACTOR = "1";
   };
 
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
