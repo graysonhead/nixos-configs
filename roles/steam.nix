aef4da865d0cdc5d20b96c6ed437a6b0374e5afd
Grayson Head <grayson@graysonhead.net>
2026-02-09 23:38:17 -0600

    Steam Fixes

--- roles/steam.nix
+++ roles/steam.nix
@@ -4,5 +4,13 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     localNetworkGameTransfers.openFirewall = true;
+    extest.enable = true; # Better input handling for streaming
   };
+
+  # Ensure VA-API is available for hardware encoding with AMD GPU
+  hardware.graphics.extraPackages = with pkgs; [
+    libva
+    libva-utils
+  ];
+
 }
